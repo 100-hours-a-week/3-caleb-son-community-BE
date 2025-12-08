6e8198c891bc4b5ce587ed9a5a4639bddb52e5e4
Gunddol <gunddol1004@naver.com>
2025-12-09 04:40:06 +0900

    feat: Add JPA column mappings for PostLikeId fields and create backend image archive.

--- src/main/java/com/ktb/community/domain/PostLikeId.java
+++ src/main/java/com/ktb/community/domain/PostLikeId.java
@@ -1,23 +1,44 @@
 package com.ktb.community.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
 public class PostLikeId implements Serializable {
-    private Integer userId;
-    private Integer postId;
+  @Column(name = "user_id", columnDefinition = "int unsigned")
+  private Integer userId;
 
-    public PostLikeId() { }
-    public PostLikeId(Integer userId, Integer postId) { this.userId = userId; this.postId = postId; }
+  @Column(name = "post_id", columnDefinition = "int unsigned")
+  private Integer postId;
 
-    public Integer getUserId() { return userId; }
-    public Integer getPostId() { return postId; }
+  public PostLikeId() {
+  }
 
-    @Override public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PostLikeId other)) return false;
-        return java.util.Objects.equals(userId, other.userId) && java.util.Objects.equals(postId, other.postId);
-    }
-    @Override public int hashCode() { return java.util.Objects.hash(userId, postId); }
+  public PostLikeId(Integer userId, Integer postId) {
+    this.userId = userId;
+    this.postId = postId;
+  }
+
+  public Integer getUserId() {
+    return userId;
+  }
+
+  public Integer getPostId() {
+    return postId;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (!(o instanceof PostLikeId other))
+      return false;
+    return java.util.Objects.equals(userId, other.userId) && java.util.Objects.equals(postId, other.postId);
+  }
+
+  @Override
+  public int hashCode() {
+    return java.util.Objects.hash(userId, postId);
+  }
 }
